eb39472c31fd87678c513fe3babc50b97861775a
Reshmi R S <44020004+reshmisaji@users.noreply.github.com>
2018-10-27 18:46:09 +0530

    modify the assert test cases for triangle

--- test/testsUsingAsserts.js
+++ test/testsUsingAsserts.js
@@ -2,6 +2,8 @@ const assert = require('assert');
 const triangleLibrary = require('../src/triangle.js');
 const {triangleRight,triangleLeft} = triangleLibrary;
 
-assert.deepEqual(triangleRight(1),'*');
-assert.deepEqual(triangleRight(2),' *\n**');
-assert.deepEqual(triangleRight(3),'  *\n **\n***');
+assert.deepEqual(triangleRight(1),'*',"Triangle Right with height 1");
+assert.deepEqual(triangleRight(2),' *\n**',"Triangle Right with height 2");
+assert.deepEqual(triangleRight(3),'  *\n **\n***',"Triangle with height 3");
+
+
