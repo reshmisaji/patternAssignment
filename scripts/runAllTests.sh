9ba6651a7abddbd29f1585f08890726f4b201112
Reshmi R S <44020004+reshmisaji@users.noreply.github.com>
2018-10-30 11:50:13 +0530

    change file names from camel case to snake case

--- scripts/runAllTests.sh
+++ scripts/runAllTests.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 set -e
-./run_test.sh ../triangleMain.js ../appTestData/input/inputFileLeft ../appTestData/output/expectedOutputLeft
-./run_test.sh ../triangleMain.js ../appTestData/input/inputFileRight ../appTestData/output/expectedOutputRight
+./run_test.sh ../triangle_main.js ../appTestData/input/inputFileLeft ../appTestData/output/expectedOutputLeft
+./run_test.sh ../triangle_main.js ../appTestData/input/inputFileRight ../appTestData/output/expectedOutputRight
 
-./run_test.sh ../rectangleMain.js ../appTestData/input/inputFileFilledRectangle ../appTestData/output/expectedOutputFilledRectangle
-./run_test.sh ../rectangleMain.js ../appTestData/input/inputFileEmptyRectangle ../appTestData/output/expectedOutputEmptyRectangle
-./run_test.sh ../rectangleMain.js ../appTestData/input/inputFileAlternatingRectangle ../appTestData/output/expectedOutputAlternatingRectangle
+./run_test.sh ../rectangle_main.js ../appTestData/input/inputFileFilledRectangle ../appTestData/output/expectedOutputFilledRectangle
+./run_test.sh ../rectangle_main.js ../appTestData/input/inputFileEmptyRectangle ../appTestData/output/expectedOutputEmptyRectangle
+./run_test.sh ../rectangle_main.js ../appTestData/input/inputFileAlternatingRectangle ../appTestData/output/expectedOutputAlternatingRectangle
 
-./run_test.sh ../diamondMain.js ../appTestData/input/inputFileFilled ../appTestData/output/expectedOutputFilled
-./run_test.sh ../diamondMain.js ../appTestData/input/inputFileHollow ../appTestData/output/expectedOutputHollow
-./run_test.sh ../diamondMain.js ../appTestData/input/inputFileAngle ../appTestData/output/expectedOutputAngle
+./run_test.sh ../diamond_main.js ../appTestData/input/inputFileFilled ../appTestData/output/expectedOutputFilled
+./run_test.sh ../diamond_main.js ../appTestData/input/inputFileHollow ../appTestData/output/expectedOutputHollow
+./run_test.sh ../diamond_main.js ../appTestData/input/inputFileAngle ../appTestData/output/expectedOutputAngle
