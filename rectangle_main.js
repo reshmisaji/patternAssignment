8473ea3a33a3b5cffcbd4e2999e3d89ed963cdf0
Reshmi R S <44020004+reshmisaji@users.noreply.github.com>
2018-10-30 12:49:52 +0530

    modify main function of rectangle

--- rectangle_main.js
+++ rectangle_main.js
@@ -2,9 +2,9 @@
 const rectangleLibrary = require('./src/rectangle.js');
 const {generateRectangle} = rectangleLibrary;
 
-const main = function(type = process.argv[2],height = +process.argv[3],width = +process.argv[4]){
+const main = function(type,width){
   console.log(generateRectangle(width,height,type));
 }
 
-main();
+main(process.argv[2],process.argv[3]);
 
