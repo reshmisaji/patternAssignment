d2d87b1e6386fb9f946c49ae11df21a10a00c3e7
Reshmi R S <44020004+reshmisaji@users.noreply.github.com>
2018-10-30 08:41:02 +0530

    moved all input and output files to input and output directories of appTestData

--- runAllTests.sh
+++ runAllTests.sh
@@ -1,13 +1,12 @@
 #! /bin/bash
 set -e
-trap 'echo failed' EXIT
-./run_test.sh ./src/triangleMain.js ./test/inputFileLeft ./test/expectedOutputLeft
-./run_test.sh ./src/triangleMain.js ./test/inputFileRight ./test/expectedOutputRight
+./run_test.sh ./src/triangleMain.js ./appTestData/input/inputFileLeft ./appTestData/output/expectedOutputLeft
+./run_test.sh ./src/triangleMain.js ./appTestData/input/inputFileRight ./appTestData/output/expectedOutputRight
 
-./run_test.sh ./src/rectangleMain.js ./test/inputFileFilledRectangle ./test/expectedOutputFilledRectangle
-./run_test.sh ./src/rectangleMain.js ./test/inputFileEmptyRectangle ./test/expectedOutputEmptyRectangle
-./run_test.sh ./src/rectangleMain.js ./test/inputFileAlternatingRectangle ./test/expectedOutputAlternatingRectangle
+./run_test.sh ./src/rectangleMain.js ./appTestData/input/inputFileFilledRectangle ./appTestData/output/expectedOutputFilledRectangle
+./run_test.sh ./src/rectangleMain.js ./appTestData/input/inputFileEmptyRectangle ./appTestData/output/expectedOutputEmptyRectangle
+./run_test.sh ./src/rectangleMain.js ./appTestData/input/inputFileAlternatingRectangle ./appTestData/output/expectedOutputAlternatingRectangle
 
-./run_test.sh ./src/diamondMain.js ./test/inputFileFilled ./test/expectedOutputFilled
-./run_test.sh ./src/diamondMain.js ./test/inputFileHollow ./test/expectedOutputHollow
-./run_test.sh ./src/diamondMain.js ./test/inputFileAngle ./test/expectedOutputAngle
+./run_test.sh ./src/diamondMain.js ./appTestData/input/inputFileFilled ./appTestData/output/expectedOutputFilled
+./run_test.sh ./src/diamondMain.js ./appTestData/input/inputFileHollow ./appTestData/output/expectedOutputHollow
+./run_test.sh ./src/diamondMain.js ./appTestData/input/inputFileAngle ./appTestData/output/expectedOutputAngle
